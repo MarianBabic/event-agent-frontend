572e19d738e7fa8150d6cd3f78d2de2acaef25e9
Marian <marian.babic@hotmail.sk>
2017-12-26 18:59:20 +0100

    components, pipe and services created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-
+// components
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ManageEventsComponent } from './manage-events/manage-events.component';
+import { ManageResourcesComponent } from './manage-resources/manage-resources.component';
+import { ViewEventsComponent } from './view-events/view-events.component';
 
+// pipes
+import { HighlightPipe } from './pipes/highlight.pipe';
+
+// services
+import { DummyDataService } from './services/dummy-data.service';
+import { RestService } from './services/rest.service';
+
+const appRoutes: Routes = [
+    { path: 'home', component: ViewEventsComponent },
+    { path: 'manage-events', component: ManageEventsComponent },
+    { path: 'manage-resources', component: ManageResourcesComponent },
+    { path: '**', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        ManageEventsComponent,
+        ManageResourcesComponent,
+        ViewEventsComponent,
+        HighlightPipe
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        RouterModule.forRoot(
+            appRoutes
+        )
     ],
-    providers: [],
+    providers: [DummyDataService, RestService],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
