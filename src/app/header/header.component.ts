2f69def214ee626988f3032f6053311722ad38e0
Marian <marian.babic@hotmail.sk>
2018-01-03 22:59:00 +0100

    confirmation message functionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SharedDataService } from '../services/shared-data.service';
+
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-    constructor() { }
+    constructor(public sharedDataService: SharedDataService) { }
 
     ngOnInit() {
     }
--- src/app/services/shared-data.service.ts
+++ src/app/services/shared-data.service.ts
@@ -3,6 +3,20 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SharedDataService {
 
+    confirmationMessage: string = '';
+
     constructor() { }
+
+    isConfirmationMessageShown(): boolean {
+        return this.confirmationMessage.length !== 0;
+    }
+
+    closeConfirmationMessage(): void {
+        this.confirmationMessage = '';
+    }
+
+    showConfirmationMessage(message: string): void {
+        this.confirmationMessage = message;
+    }
 
 }
