92eb635193394652ad53d038ca78f6ec943fa9f4
Marian <marian.babic@hotmail.sk>
2018-01-30 23:21:34 +0100

    manage events component

--- src/app/manage-events/pair/pair.component.ts
+++ src/app/manage-events/pair/pair.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
+import { DummyDataService } from '../../services/dummy-data.service';
+import { SharedDataService } from '../../services/shared-data.service';
+
 @Component({
     selector: 'app-pair',
     templateUrl: './pair.component.html',
@@ -13,13 +16,43 @@ export class PairComponent implements OnInit {
     @Input() hasForm: boolean;
     @ViewChild('form') private form: NgForm;
 
-    constructor() { }
+    constructor(private sharedDataService: SharedDataService, private dummyDataService: DummyDataService) { }
 
     ngOnInit() { }
+
+    isFormValid(): boolean {
+        return this.form['name'] && this.form['description'] && this.form['start_time'] && this.form['end_time'] && this.form['placeName'];
+    }
 
     // TODO
     onSubmit(index: number): void {
         console.log(this.form);
+        this.deletePair();
+        // TODO: scroll to the top so message is visible
+        this.sharedDataService.confirmationMessage = `You have submitted the hinted pair of events: '${this.pair[0].name}' & '${this.pair[1].name}'`;
+    }
+
+    onDelete(): void {
+        // TODO: call rest to delete the pair from the server
+        this.deletePair();
+        // TODO: scroll to the top so message is visible
+        this.sharedDataService.confirmationMessage = `You have deleted the hinted pair of events: '${this.pair[0].name}' & '${this.pair[1].name}'`;
+    }
+
+    // deleted the pair of events locally from the browser
+    private deletePair(): void {
+        if (this.hasForm) {
+            for (let i = this.dummyDataService.dummySimilarEvents[0].length - 1; i >= 0; i--) {
+                if (this.dummyDataService.dummySimilarEvents[0][i][0].id === this.pair[0].id && this.dummyDataService.dummySimilarEvents[0][i][1].id === this.pair[1].id) {
+                    this.dummyDataService.dummySimilarEvents[0].splice(i, 1);
+                }
+            }
+        } else {
+            for (let i = this.dummyDataService.dummySimilarEvents[1].length - 1; i >= 0; i--) {
+                if (this.dummyDataService.dummySimilarEvents[1][i][0].id === this.pair[0].id && this.dummyDataService.dummySimilarEvents[1][i][1].id === this.pair[1].id)
+                    this.dummyDataService.dummySimilarEvents[1].splice(i, 1);
+            }
+        }
     }
 
     // TODO
--- src/app/services/dummy-data.service.ts
+++ src/app/services/dummy-data.service.ts
@@ -28,9 +28,9 @@ export class DummyDataService {
         {
             "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
             "end_time": "2017-11-01T11:00:00+0200",
-            "name": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "name": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
             "place": {
-                "name": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                "name": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
                 "location": {
                     "city": "Košice",
                     "country": "Slovakia",
