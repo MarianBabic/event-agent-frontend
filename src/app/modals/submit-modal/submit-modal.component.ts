0d6d7c291dc8d6c1d06dccde29171c00896cd162
Marian <marian.babic@hotmail.sk>
2018-01-31 00:23:43 +0100

    submit modal component

--- src/app/modals/submit-modal/submit-modal.component.ts
+++ src/app/modals/submit-modal/submit-modal.component.ts
@@ -9,8 +9,26 @@ import { SharedDataService } from '../../services/shared-data.service';
 })
 export class SubmitModalComponent implements OnInit {
 
+    // events are hinted are identical if true, or nested otherwise
+    isHintTypeIdentical: boolean = true;
+
     constructor(public sharedDataService: SharedDataService) { }
 
     ngOnInit() { }
+
+    setHintType(isHintTypeIdentical): void {
+        this.isHintTypeIdentical = isHintTypeIdentical;
+    }
+
+    onCancel(): void {
+        this.sharedDataService.closeSubmitModal();
+    }
+
+    onSubmit(): void {
+        this.sharedDataService.closeSubmitModal();
+        // TODO: zobrazit mena eventov namiesto ich id
+        let idsIiterator = this.sharedDataService.submitModal.events.values();
+        this.sharedDataService.confirmationMessage = `You have hinted 2 events as similar: 'id1: ${idsIiterator.next().value}' & 'id2: ${idsIiterator.next().value}'`;
+    }
 
 }
