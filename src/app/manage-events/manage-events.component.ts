3298ffdfcc6caad048b977557c05afe23a484c74
Marian <marian.babic@hotmail.sk>
2018-02-23 23:20:59 +0100

    changes

--- src/app/manage-events/manage-events.component.ts
+++ src/app/manage-events/manage-events.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { DummyDataService } from '../services/dummy-data.service';
+import { RestService } from '../services/rest.service';
 
 @Component({
     selector: 'app-manage-events',
@@ -11,10 +12,18 @@ export class ManageEventsComponent implements OnInit {
 
     similarEvents: any[];
 
-    constructor(private dummyDataService: DummyDataService) { }
+    constructor(private dummyDataService: DummyDataService, private restService: RestService) { }
 
     ngOnInit() {
         this.similarEvents = this.dummyDataService.dummySimilarEvents;
+        this.restService.getUnsolvedSimilarities().subscribe(
+            data => {
+                console.log(data);
+            },
+            err => {
+                alert('An error encountered while loading data from server! Please try again.');
+            }
+        );
     }
 
 }
